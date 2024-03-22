721fd466a8728d70348dbdd3361555d811458aab
JyojiTenguam <j.y.oji@hotmail.com>
2024-03-22 16:35:31 -0300

    Resolvendo requisito 04 de Project React Testing Library TS

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('NotFound component tests', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<App />, { route: '/NotFound' });
+
+    const heading = screen.getByRole('heading', { name: /Page requested not found/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('verifica se uma imagem com o src específico é renderizada', () => {
+    renderWithRouter(<App />, { route: '/NotFound' });
+
+    const image = screen.getByRole('img', { name: /Clefairy pushing buttons randomly with text I have no idea what i'm doing/i }) as HTMLImageElement;
+
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain('404.gif');
+  });
+});
